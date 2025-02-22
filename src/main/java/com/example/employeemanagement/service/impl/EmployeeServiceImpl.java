20ea4b98926c62c8a6186bb511c515b474f98e8f
Huynh Thi Hong Thanh <thanh.huynh.9111994@gmail.com>
2025-02-23 00:01:01 +0700

    Change Add and Update Employee Feature - Many to One relationship in Spring Boot

--- src/main/java/com/example/employeemanagement/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/example/employeemanagement/service/impl/EmployeeServiceImpl.java
@@ -1,30 +1,38 @@
 package com.example.employeemanagement.service.impl;
 
 import com.example.employeemanagement.dto.EmployeeDto;
+import com.example.employeemanagement.entity.Department;
 import com.example.employeemanagement.entity.Employee;
 import com.example.employeemanagement.exception.ResourceNotFoundException;
 import com.example.employeemanagement.mapper.EmployeeMapper;
+import com.example.employeemanagement.repository.DepartmentRepository;
 import com.example.employeemanagement.repository.EmployeeRepository;
 import com.example.employeemanagement.service.EmployeeService;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
 
-    // TODO: Add @RequiredArgsConstructor
-    // TODO: Add final front of EmployeeRepository
+    private EmployeeRepository employeeRepository;
 
-    private final EmployeeRepository employeeRepository;
+    private DepartmentRepository departmentRepository;
 
     @Override
     public EmployeeDto createEmployee(EmployeeDto employeeDto) {
         Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+
+        // TODO: Many To One Relationship in Spring Boot
+        Department department = departmentRepository.findById(employeeDto.getDepartmentId())
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Department not found"));
+        employee.setDepartment(department);
+
         Employee savedEmployee = employeeRepository.save(employee);
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
     }
@@ -53,6 +61,12 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setFirstName(updatedEmployee.getFirstName());
         employee.setLastName(updatedEmployee.getLastName());
         employee.setEmail(updatedEmployee.getEmail());
+
+        // TODO: Many To One Relationship in Spring Boot
+        Department department = departmentRepository.findById(updatedEmployee.getDepartmentId())
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Department not found"));
+        employee.setDepartment(department);
 
         Employee updateEmployeeObj = employeeRepository.save(employee);
         return EmployeeMapper.mapToEmployeeDto(updateEmployeeObj);
